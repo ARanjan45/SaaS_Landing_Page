c33fa4cbadd36f34af3b8a1996bc442937e5444f
Aprajita <beliefinaprajita@gmail.com>
2026-04-17 09:42:10 +0530

    Header and Hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import {Header} from "@/sections/Header";
+import {Hero} from "@/sections/Hero";
+import {LogoTicker} from "@/sections/LogoTicker";
 export default function Home() {
-  return <Header/>;
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <LogoTicker/>
+    </>
+  );
 }
