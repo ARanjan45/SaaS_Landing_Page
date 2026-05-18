2cecc118fa6225e5f19c26c2fefd3d6a91583356
Aprajita <beliefinaprajita@gmail.com>
2026-05-18 22:39:55 +0530

    Fixed all built errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Landing Page - SaaS",
+  description: "Frontend Template for SaaS Landing Page built with Next.js and Tailwind CSS",
 };
 
 export default function RootLayout({
